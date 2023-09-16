e210b69271aaa9054220ed67cf801a6bc5729e18
faharetana-rkt <rakotomaharitra@gmail.com>
2023-09-16 17:02:02 +0300

    Finished Prep01 and Prep 02

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+		string textGrade = Console.ReadLine();
+		int grade = int.Parse(textGrade);
+
+		string letter = "";
+		string sign = "";
+
+		if (grade >= 90)
+		{
+			letter = "A";
+		}
+		else if (grade >= 80 && grade < 90)
+		{
+			letter = "B";
+		}
+		else if (grade >= 70 && grade < 80)
+		{
+			letter = "C";
+		}
+		else if (grade >= 60 && grade < 70)
+		{
+			letter = "D";
+		}
+		else
+		{
+			letter = "F";
+		}
+
+		if ((grade % 10) >=7 )
+		{
+			if (grade >= 90 || grade <= 60)
+			{
+				sign = "";
+			}
+			else
+			{
+				sign = "+";
+			}
+		}
+		else if ((grade % 10) <3)
+		{
+			if (grade >= 90 || grade <= 60)
+			{
+				sign = "";
+			}
+			else
+			{
+				sign = "-";
+			}
+		}
+		else
+		{
+			sign = "";
+		}
+
+		Console.WriteLine($"Your grade is {letter}{sign}.");
+
+		if (grade >= 70)
+		{
+			Console.WriteLine("Congrats you passed the class with flying colors.");
+		}
+		else
+		{
+			Console.WriteLine("Unfortunately you didn't pass the class, please try again next semester.");
+		}
     }
 }
