0348ada3857e965457e5e8be0436133774290c07
faharetana-rkt <rakotomaharitra@gmail.com>
2023-10-25 21:00:38 +0300

    Finished Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment maths = new MathAssignment("7.3", "8-19", "Roberto Rodriguez", "Fractions");
+        Console.WriteLine(maths.GetSummary());
+        Console.WriteLine(maths.GetHomeWorkList());
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+
     }
 }
