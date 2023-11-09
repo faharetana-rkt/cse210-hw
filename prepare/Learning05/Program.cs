4fc4343667de61679640fd4d282dae23cd85f823
faharetana-rkt <rakotomaharitra@gmail.com>
2023-11-09 21:32:26 +0300

    Finished the shape learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square(2.5, "Blue");
+        //Console.WriteLine(square.GetColor());
+        //Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle(10, 5, "Red");
+        //Console.WriteLine(rectangle.GetColor());
+        //Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle(6.5, "Black");
+        //Console.WriteLine(circle.GetColor());
+        //Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(square);
+        shapeList.Add(rectangle);
+        shapeList.Add(circle);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"The Color is {shape.GetColor()} and the area is {shape.GetArea()}");
+        }
     }
 }
