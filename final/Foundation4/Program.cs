5ea9edd07b7305d8c1a6bc6d426f6da259408fe2
faharetana-rkt <rakotomaharitra@gmail.com>
2023-11-29 06:14:23 +0300

    modified the swimming laps and duration

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -8,7 +8,7 @@ class Program
 		
 		Running run = new("27 Nov 2023", 40, 5.5);
 		activities.Add(run);
-		Swimming swim = new("28 Nov 2023", 30, 3);
+		Swimming swim = new("28 Nov 2023", 15, 5);
 		activities.Add(swim);
 		Cycling cycle = new("29 Nov 2023", 30, 15);
 		activities.Add(cycle);
